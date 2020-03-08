a3b096b391fc7b5c0ae8ff90d8ae0ab804c104ab
JasonAg <jason.aguilera.s@gmail.com>
2020-03-08 16:26:28 -0400

    Added wait calls along with a thread sleep to click find food button

--- src/test/java/Pages/homePage.java
+++ src/test/java/Pages/homePage.java
@@ -1,36 +1,75 @@
 package Pages;
 
+import java.util.NoSuchElementException;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class homePage {
 	
 	private WebDriver driver;
+	private WebDriverWait wait;
+
+	private WebElement _findFood;
+	private WebElement _address;
 	
-	@FindBy(xpath = "//*[@id=\"location-typeahead-home-input\"]")
-	private WebElement address;
-	
-	@FindBy(xpath = "//*[@id=\"wrapper\"]/main/div[1]/div[2]/div/button")
-	private WebElement findFood;
-	
-	public homePage(WebDriver driver) {
+	public homePage(WebDriver driver, WebDriverWait wait) {
 		this.driver = driver;
+		this.wait = wait;
 		PageFactory.initElements(driver, this);
+		
 	}
 	
 	public void openHomePage() {
 		driver.get("https://www.ubereats.com/");
+		
 	}
 	
-	public void insertAddress(String inputAddress) {
-		//WebElement address = driver.findElement(_address);
-		address.sendKeys(inputAddress);
+	public void insertAddress(String inputAddress) throws NoSuchElementException {
+		try {
+			_address = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"location-typeahead-home-input\"]")));
+			
+		} catch (NoSuchElementException e) {
+			System.out.println("Address bar not found!");
+			
+		}
+		if (_address != null) {
+			_address.clear();
+			_address.click();
+			_address.sendKeys(inputAddress);
+			
+		}
 	}
 	
-	public void clickFindFood() {
-		//WebElement findFood = driver.findElement(_findFood);
-		findFood.click();
+	public void clickFindFood() throws NoSuchElementException, InterruptedException {
+		try {
+			_findFood = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"wrapper\"]/main/div[1]/div[2]/div/button")));
+			
+		} catch (NoSuchElementException e) {
+			System.out.println("Find food button not found!");
+			
+		}
+		
+		if (_findFood != null) {
+			try {
+				Thread.sleep(3000);
+				
+			} catch (InterruptedException e) {
+				System.out.println("Exception interrupted");
+				
+			}
+			
+			_findFood.click();
+		}
+	}
+	
+	public void checkIfRedirected() {
+		
 	}
 }
--- src/test/java/StepDefs/stepDefs.java
+++ src/test/java/StepDefs/stepDefs.java
@@ -1,9 +1,13 @@
 package StepDefs;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import Pages.*;
 
@@ -14,32 +18,37 @@ import io.cucumber.java.en.When;
 public class stepDefs {
 	
 	private WebDriver driver;
-	//private WebDriverSingleton webdriversingleton;
+	private WebDriverWait wait;
+	
 	private homePage _homePage;
 	
 	@Given("user goes to UberEats web page")
 	public void user_goes_to_UberEats_web_page() {
-		System.out.println("User goes to www.ubereats.com/");
-		 
-		//webdriversingleton = WebDriverSingleton.getInstanceOfWebDriverSingleton();
-		driver = WebDriverSingleton.getInstanceOfWebDriverSingleton().getWebDriver();
+		//System.out.println("User goes to www.ubereats.com/");
 		
-		_homePage = new homePage(driver);
+		driver = WebDriverSingleton.getInstanceOfWebDriverSingleton().getWebDriver();
+		driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		wait = new WebDriverWait(driver, 8);
+		
+		_homePage = new homePage(driver, wait);
 		_homePage.openHomePage();
 	}
 
 	@Given("user enters a delivery address")
 	public void user_enters_a_delivery_address() {
+		//System.out.println("User enters a delivery address");
 	    _homePage.insertAddress("New york 10001");
 	}
 	
 	@When("user clicks Find Food button")
 	public void user_clicks_Find_Food_button() {
+		System.out.println("User clicks Find food button");
 	    _homePage.clickFindFood();
 	}
 
 	@Then("user is redirected to search results")
 	public void user_is_redirected_to_search_results() {
-		System.out.println("User is redirected");
+		
 	}
 }
